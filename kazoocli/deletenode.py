f9feb15c1947d5845fed5095f9f4faf64a69970e
nhanpt5 <nhanpt5@CPU10876-local>
2015-07-14 15:33:28 +0700

    remove some code

--- kazoocli/deletenode.py
+++ kazoocli/deletenode.py
@@ -1,14 +1,33 @@
 import sys
-sys.path.append('kazoolib')
-from kazoo.client import KazooClient
-server = '10.30.14.50:2183'
-node = '/dbg/trans/atm/'
+import time
+import datetime
+try:
+    sys.path.append('kazoolib')
+    from kazoo.client import KazooClient
+except Exception:
+        print "[Error] Please cd to project directory :). If still error please contact nhanpt5@vng.com.vn."
+        sys.exit(2)
+#server = '10.30.14.50:2183'
+#node = '/dbg/trans/atm/'
+node = '/nhanpt5/'
+server = 'localhost:2181'
+
+def check(a):
+    value = time.strptime(a,'%Y%m%d%H%M%S%f')
+    now = time.localtime()
+    delta = time.mktime(now) - time.mktime(value)
+    # kiem tra node da ton tai hon 7 ngay
+    res = datetime.timedelta(seconds=delta).days
+    if res >= 7:
+	return  True
+
 try:
     zk = KazooClient(hosts=server)
     zk.start()
     for child in zk.get_children(node):
-        print child
-	#zk.delete(node + child)
+	if check(child):
+	    print child
+	    #zk.delete(node + child)
     zk.stop()
 except Exception as ex:
     print ex
